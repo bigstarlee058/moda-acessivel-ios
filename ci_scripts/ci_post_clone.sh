69d343a8e67518d07845b5ecbb2de267e11068a9
2893viana-design <2893viana@gmail.com>
2026-08-15 19:21:56 -0300

    Update ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+# Garante permissão de execução
+chmod +x "$0"
+
+# Força a exportação exclusiva para a App Store no Xcode Cloud
 defaults write com.apple.dt.xcodebuild.exportArchive ExportAppStoreOnly -bool true
+
+echo "Script de pós-clone executado com sucesso para distribuição na App Store."
