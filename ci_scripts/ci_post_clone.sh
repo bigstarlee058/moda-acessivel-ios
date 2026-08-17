4ef5169286cc361a9305082729d36cf8e30ab408
Thaisa <viana_thaisa@hotmail.com>
2026-08-17 20:55:14 -0300

    fix: remove invalid defaults export override

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,6 +4,5 @@
 chmod +x "$0"
 
 # Força a exportação exclusiva para a App Store no Xcode Cloud
-defaults write com.apple.dt.xcodebuild.exportArchive ExportAppStoreOnly -bool true
 
 echo "Script de pós-clone executado com sucesso para distribuição na App Store."
